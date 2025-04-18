a94a7c8e20ece3277e84233d1cb9b04d37176efb
NA-Asraful-Khan <soyonshikhor@gmail.com>
2025-04-18 22:06:22 +0600

    Initial Setup

---
+++ Fundamentals/001_Introduction.js
@@ -0,0 +1,1 @@
+// https://www.youtube.com/watch?v=H5BK6DhfWy4&list=PL_XxuZqN0xVAu_dWUVFbscqZdTzE8t6Z1&index=3
---
+++ Fundamentals/003_How_To_Use.js
@@ -0,0 +1,30 @@
+//~ Always Use NoteBook
+//? Always Type Code in Editor
+//! Find Related Examples and Create Example
+//^ Spend Some Moment on Each Chapter
+//& Discuss With Others What You Have Learned
+//*
+
+/* TODO
+//* Programming Basics
+//* Logic and Conditions
+//* Loops and Repetition
+//* Handling Strings
+//* Arrays
+//* Objects
+//* Functions
+//* Functional Programming
+//* Scope and Closure
+//* OOP Intro
+//* OOP with JS
+//* Prototype
+//* Prototypical Inheritance
+//* ES6
+//* Error Handling
+//* Asynchronous Programming
+//* DOM
+//* Project
+//* Javascript Libraries
+//* Future Roadmap
+
+*/
