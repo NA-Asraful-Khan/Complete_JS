3ca005c31be1469b74f4788647896933142ad31d
NA-Asraful-Khan <soyonshikhor@gmail.com>
2025-04-24 06:03:00 +0600

    Add foundational JavaScript files for statements and math functions

---
+++ Fundamentals/016_JS_Statements.js
@@ -0,0 +1,9 @@
+console.log("Hello World");
+
+var str = "Hello World";
+var number = 10 + (30 / 5) * 5;
+
+var a = 10;
+var b = 10;
+
+var c = a >= b;
---
+++ Fundamentals/017_JS_Math_Function.js
@@ -0,0 +1,23 @@
+console.log(Math.E);
+
+console.log(Math.PI);
+var n = 4.589;
+
+console.log(n);
+console.log(Math.abs(n));
+console.log(Math.floor(n));
+console.log(Math.ceil(n));
+console.log(Math.round(n));
+
+console.log(Math.max(1, 2, 3, 4, 5));
+console.log(Math.min(1, 2, 3, 4, 5));
+
+console.log(Math.pow(2, 3));
+console.log(Math.pow(3, 2));
+
+console.log(Math.sqrt(16));
+console.log(Math.sqrt(4));
+
+console.log(Math.random());
+
+console.log(Math.ceil(Math.random() * 5));
